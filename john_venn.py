0f473badec45432394671a22dbd112e985d076cd
RainTreeCrow <1683762615@qq.com>
2024-09-20 18:25:54 +0100

    BlackRock

---
+++ john_venn.py
@@ -0,0 +1,34 @@
+import sys
+
+common_list = []
+line = input()
+
+while line != "":
+    new_list = sorted(line.replace("\n", "").split(" "))
+    # print(new_list)
+    if common_list == []:
+        common_list = new_list
+    else:
+        find_common = []
+        for item in common_list:
+            # print("item:", item)
+            for new_item in new_list:
+                # print("new item", new_item)
+                if item == new_item:
+                    find_common.append(item)
+                    # print("found", item)
+                    break
+                elif item < new_item:
+                    break
+        common_list = find_common
+    line = input()
+
+if common_list == []:
+    result = "NULL"
+
+else:
+    result = common_list[0]
+    for item in common_list[1:]:
+        result += " " + item
+
+print(result)
---
+++ road_trip.py
@@ -0,0 +1,22 @@
+import sys
+
+distances = {}
+
+line = input()
+pairs = line.replace(" ", "").split(";")
+for the_pair in pairs:
+    pair = the_pair.split(",")
+    if len(pair) == 2:
+        city, distance = pair[0], int(pair[1])
+        distances[city] = distance
+
+sorted_distance = sorted(distances.items(), key=lambda item: item[1])
+last_stop = sorted_distance[0][1]
+result = str(last_stop)
+
+for pair in sorted_distance[1:]:
+    current_stop = pair[1]
+    result += "," + str(current_stop - last_stop)
+    last_stop = current_stop
+
+print(result)
